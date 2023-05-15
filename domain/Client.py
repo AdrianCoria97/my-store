c81fdf778e1cc282b725a9bdf2294996749d21c1
AdrianCoria97 <coriaadrian4@gmail.com>
2023-05-15 10:53:48 -0300

    Adding setters and getters

--- domain/Client.py
+++ domain/Client.py
@@ -23,11 +23,58 @@ class Client:
         self.email = email
         self.status = status
 
-    # hacer setter y getter
-
+    # GETTERS
     def get_name(self):
         return self.name
 
+    def get_last_name(self):
+        return self.last_name
+
+    def get_age(self):
+        return self.age
+
+    def get_address(self):
+        return self.address
+
+    def get_city(self):
+        return self.city
+
+    def get_zip_code(self):
+        return self.zip_code
+
+    def get_phone(self):
+        return self.phone
+
+    def get_email(self):
+        return self.email
+
+    def get_status(self):
+        return self.status
+
+    # SETTERS
     def set_name(self, new_name):
         self.name = new_name
 
+    def set_last_name(self, new_last_name):
+        self.last_name = new_last_name
+
+    def set_age(self, new_age):
+        self.age = new_age
+
+    def set_address(self, new_address):
+        self.address = new_address
+
+    def set_city(self, new_city):
+        self.city = new_city
+
+    def set_zip_code(self, new_zip_code):
+        self.zip_code = new_zip_code
+
+    def set_phone(self, new_phone):
+        self.phone= new_phone
+
+    def set_email(self, new_email):
+        self.email = new_email
+
+    def set_status(self, new_status):
+        self.status = new_status
--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from domain.Buyer import Buyer
 if __name__ == '__main__':
     first_buyer = Buyer(purchase_history=[], shopping_cart=[], shipping_address="Calle none 123", has_subscribed=True,
                         id=str(uuid.uuid4()), name="Mauricio", last_name="Pizarro", age=26, address="La Colonia",
-                        city="Mendoza", zip_code="5572", phone="2634773945", email="maurigato@gmail.com", status=False)
+                        city="Mendoza", zip_code="5572", phone="2634773945", email="maurigato@gmail.com", status=True)
 
-    first_article = Article( id=str(uuid.uuid4()), name= "Philco", item_id=1, description= "TV Smart",
-                             category="Electrodomesticos", price=100000.00,
-                             stock=3, seller=first_buyer, average_rating=10, comments=["Muy Bueno", "Exelente"],
-                             date_created=datetime.now(), shipping_methods=["Andreani", "Envio Express"],
-                             buyer=first_buyer)
+    first_article = Article(id=str(uuid.uuid4()), name= "Philco", item_id=1, description="TV Smart",
+                            category="Electrodomesticos", price=100000.00, stock=3, seller=first_buyer,
+                            average_rating=10, comments=["Muy Bueno", "Exelente"], date_created=datetime.now(),
+                            shipping_methods=["Andreani", "Envio Express"], buyer=first_buyer)
 
     print(first_article.__dict__)
     print(first_buyer.__dict__)
